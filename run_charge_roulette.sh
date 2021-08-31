abca6031e7ebf2ce1223e37552d6a640b40cb373
andymlau <andy.m.lau@outlook.com>
2021-08-31 19:33:40 +0100

    Added argument handling for main script and added more functionalities

--- run_charge_roulette.sh
+++ run_charge_roulette.sh
@@ -1,13 +1,11 @@
 #! /usr/bin/env bash
 
-# ChargeRoulette script from https://github.com/andymlau/ChargeRoulette
-# Written by Andy M Lau, PhD (2021)
+# ChargeRoulette: Automating GROMACS pdb2gmx for gas phase MD simulations
+# Downloaded from: https://github.com/andymlau/ChargeRoulette
+# Author: Andy M. Lau, PhD (2021) (andy.m.lau@ucl.ac.uk)
+# See LICENSE file for more details.
 
 set -eu
-
-if [ $# -ne 4 ]; then
-  echo "$0: Usage: ./run_pdb2gmx_autoCharge.sh <input.pdb; path> <n_charges; int> <n_samples; int> <out_dir; path>"
-fi
 
 progdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 scripts="${progdir}/scripts"
@@ -15,37 +13,112 @@ chargeroulette="${scripts}/charge_roulette.py"
 autoassign="${scripts}/__auto_assign.exp"
 py="$(which python)"
 
-input=$(readlink -f $1)
-ncharges=$2
-nsamples=$3
-output=$(readlink -f $4)
-
 # source /usr/local/gromacs/bin/GMXRC
 gromacs="gmx pdb2gmx"
 fflags="-v -heavyh -ff oplsaa -water none -lys -arg -asp -glu -his -ter -renum -merge all"
+
+# By default, use arg, his and lys residues
+arginine=1
+histidine=1
+lysine=1
+
+if [[ $# -eq 0 ]] ; then
+    echo "$0: No arguments given. Run ./run_charge_roulette.sh -h for more help. Exiting."
+    echo " "
+    exit 1
+fi
+
+title () {
+  echo " "
+  echo "-----------------------------------------------------------------------"
+  echo "ChargeRoulette: Automating GROMACS pdb2gmx for gas phase MD simulations"
+  echo "-----------------------------------------------------------------------"
+  echo " "
+  echo "Downloaded from: https://github.com/andymlau/ChargeRoulette"
+  echo "Author: Andy M. Lau, PhD (2021) (andy.m.lau@ucl.ac.uk)"
+  echo " "
+}
+
+usage () {
+  echo "Usage: ./$0 [-i <string; path] [-o <string; path>] [-n <int>] [-s <int>]"
+}
+
+# Input option handling
+while getopts ':i:o:n:s:c:hRHK' opt; do
+  case "$opt" in
+    h)
+    title
+    usage
+    echo " "
+    echo "-h, --help       Show this help menu."
+    echo " "
+    echo "Mandatory arguments:"
+    echo "-i, --input      (path) input pdb file to run ChargeRoulette on. Should be the (residuedepth) output of DEPTH server."
+    echo "-o, --output     (path) specify the output directory for files to be written to."
+    echo "-n, --ncharges   (int) specify the number of positive charges that each run should generate. e.g. 7 for a the output pdb to have a +7 charge."
+    echo "-s, --nsamples   (int) specify the number of alternative charge configurations. e.g. 5 will generate 5 different output pdbs."
+    echo " "
+    echo "Optional arguments:"
+    echo "-c, --custom     (path) use a custom assignment schedule. If option used, -n and -s are no longer mandatory."
+    echo "-R, --arg        Do not include arginines."
+    echo "-H, --his        Do not include histidines."
+    echo "-K, --lys        Do not include lysines."
+    echo " "
+    exit 1 ;;
+
+    i) input=$(readlink -f $OPTARG) ;;
+    o) output=$(readlink -f $OPTARG) ;;
+    n) ncharges=$OPTARG ;;
+    s) nsamples=$OPTARG ;;
+    c) custom=$(readlink -f $OPTARG) ;;
+    R) arginine=0 ;;
+    H) histidine=0 ;;
+    K) lysine=0 ;;
+    :) echo -e "Error: -$OPTARG requires an argument. See help (./$0 -h) for more details.\n"; usage; exit 1 ;;
+    # \?) echo "Unrecognised option '$*'"; exit 1;;
+    *) echo -e "Error: Unrecognised option '$*'. See help (./$0 -h) for more details.\n"; usage; exit 1 ;;
+  esac
+done
 
 # Check that gromacs command is found
 if ! command -v $gromacs &> /dev/null
 then
     echo "GROMACS (gmx pdb2gmx) was not found. Did you forget to install or source GMXRC?"
-    exit 0
+    exit 1
 fi
 
 # Check that expect is found
 if ! command -v expect &> /dev/null
 then
     echo "expect was not found. Did you forget to install?"
-    exit 0
+    exit 1
 fi
+
+mandatory () {
+  echo -e "Error: Mandatory options and arguments not set. See help (./$0 -h) for more details.\n"
+}
+
+# Check that mandatory arguments have been set
+if [ -z ${custom+x} ]; then
+  if [ -z ${input+x} ] || [ -z ${output+x} ] || [ -z ${ncharges+x} ] || [ -z ${nsamples+x} ]; then
+    mandatory; usage; exit 1
+  fi
+else
+  if [ -z ${input+x} ] || [ -z ${output+x} ]; then
+    mandatory; usage; exit 1
+  fi
+fi
+
+title
 
 # Some checks
 if test ! -f $input; then
   echo "Input file ${input} does not exist. Please check path. "
-  exit 0
+  exit 1
 else
   if test ! -s $input; then
     echo "Input file ${input} is empty. Please check input."
-    exit 0
+    exit 1
   fi
 fi
 
@@ -60,27 +133,50 @@ else
         [Nn]* ) echo "Exiting."; exit;;
         * ) echo "Please answer y or n.";;
     esac
-done
-
+  done
 fi
 
-# Run charge roulette
-echo -e "\nRunning charge_roulette.py"
-echo "  - Will sample $ncharges charges from $(basename $input) $nsamples times."
+# If custom schedule is used, check the file exists
+if [ ! -z ${custom+x} ]; then
+  if test ! -f ${custom}; then
+    echo "Schedule file ${custom} not found. Please check path."
+    exit 1
+  else
+    echo "Will use custom assignment schedule from $custom"
 
-$py $chargeroulette -i $input -n $ncharges -s $nsamples -o $output
-
-charges="$output/$(basename ${input%.pdb})_samples.txt"
-if test -f $charges; then
-  echo "  - The following charges will be assigned: (sample_no,LYS,ARG,HIS):"
-  sed 's/^/    /' $charges
+    # Copy the user given custom file and check for blank line at eof
+    charges="$output/$(basename ${custom})"
+    sed -e '$a\' $custom > $charges
+  fi
 else
-  echo "Error: charge_roulette.py output not found."
-  exit 0
+  # Run charge roulette
+  echo -e "\nWill sample $ncharges charges from $(basename $input) $nsamples times."
+
+  [[ $arginine -eq 0 ]] && echo "  ** Residue sampling will ignore all arginine residues."
+  [[ $histidine -eq 0 ]] && echo "  ** Residue sampling will ignore all histidine residues."
+  [[ $lysine -eq 0 ]] && echo "  ** Residue sampling will ignore all lysine residues."
+
+  $py $chargeroulette -i $input -n $ncharges -s $nsamples -o $output -R $arginine -H $histidine -K $lysine
+  if [ $? == 1 ]; then exit 1; fi
+
+  charges="$output/$(basename ${input%.pdb})_samples.txt"
+fi
+
+# Print summary of the charges to be assigned:
+if test ! -f $charges; then
+  echo "Error: ${charges} not found."
+  exit 1
 fi
 
 # Iterate over each charge set
 while read -r line; do
+  # Check that line has the correct format:
+  # [[ "$line" =~ ^(\d+)(,\{[\d\s]*\})(,\{[\d\s]*\})(,\{[\d\s]*\})$ ]] || echo -e "\n    Error: Incorrect formatting in schedule file."; exit 1
+  if [[ ! "$line" =~ ^([[:digit:]]+),\{.*\} ]]; then
+    echo -e "Error: Incorrect formatting in schedule file.\n"
+    exit 1
+  fi
+
   no=$(cut -d"," -f1 <<< $line)
   lys=$(cut -d"," -f2 <<< $line)
   arg=$(cut -d"," -f3 <<< $line)
@@ -110,14 +206,14 @@ while read -r line; do
     log=$(readlink -f "${jobdir}/pdb2gmx.log")
     expect $runexpect > $log
 
-    echo -e "\n  Charge set ${no}: "
+    echo -e "\nCharge set ${no}: "
     echo "  - Files in directory: ${jobdir}"
 
     # Check for fatal errors:
     if grep -q "Fatal error" $log; then
       sed -ne '/Fatal error/,$ p' $log | sed "s/^/    /g"
       echo -e "\n"
-      exit 0
+      exit 1
     else
       if grep -q "Total charge" $log; then
         echo "  - Charges assigned to residues: "
@@ -125,13 +221,14 @@ while read -r line; do
         echo "    Arginines: $(echo ${arg} | sed 's/{//g; s/}//g; s/ /, /g')"
         echo "    Histidines: $(echo ${his} | sed 's/{//g; s/}//g; s/ /, /g')"
         echo -e "\n  - $(grep 'Total charge' $log)"
+        [[ ! -z ${custom+x} ]] && echo "    ** Custom schedule used: check total charge!"
       else
         echo -e "\n  - pdb2gmx did not terminate correctly ('Total charge' not found). Please check output log of pdb2gmx for errors."
-        exit 0
+        exit 1
       fi
     fi
   fi
 
 done < $charges
 
-echo -e "\nDone."
+echo -e "\nProgram Finished."
--- scripts/charge_roulette.py
+++ scripts/charge_roulette.py
@@ -1,5 +1,7 @@
-# ChargeRoulette script from https://github.com/andymlau/ChargeRoulette
-# Written by Andy M Lau, PhD (2021)
+# ChargeRoulette: Automating GROMACS pdb2gmx for gas phase MD simulations
+# Downloaded from: https://github.com/andymlau/ChargeRoulette
+# Author: Andy M. Lau, PhD (2021) (andy.m.lau@ucl.ac.uk)
+# See LICENSE file for more details.
 
 import argparse
 from utils import *
@@ -17,6 +19,12 @@ def main():
                         help='The number of residues to select each time.')
     parser.add_argument('-s', '--n_samples', type=int, required=True,
                         help='The number of samples.')
+    parser.add_argument('-R', '--no_arg', type=int, required=False,
+                        help='Do not charge arginines.')
+    parser.add_argument('-H', '--no_his', type=int, required=False,
+                        help='Do not charge histidines.')
+    parser.add_argument('-K', '--no_lys', type=int, required=False,
+                        help='Do not charge lysines.')
     parser.add_argument('-d', '--depth', type=float, required=False, default=5.0,
                         help='(default 5.0) Charges will only be selected from residues above this depth.')
     parser.add_argument('-p', '--patience', type=int, required=False, default=10,
@@ -25,13 +33,23 @@ def main():
 
     # Open pdb, filter out non-CA atoms, get all basic residues that meet depth criteria
     pdb = select_from_mol(open_pdb(args.input), 'n', 'CA')
-    basic = select_from_mol(pdb, 'resn', ['HIS', 'ARG', 'LYS'])[0]
+
+    posres = []
+    if args.no_arg:
+        posres.append('ARG')
+    if args.no_his:
+        posres.append('HIS')
+    if args.no_lys:
+        posres.append('LYS')
+
+    basic = select_from_mol(pdb, 'resn', posres)[0]
     basic = basic[basic['b'] < args.depth]
 
     # Check that there are enough residues to choose from
     if len(basic) < args.n_charges:
-        print("Error: requested number of charges is greater than the number of residues that can be sampled from. Decrease the number of charges or check PDB.")
-        exit()
+        print(
+            f"\n    Error: requested number of charges ({args.n_charges}) is greater than the number of residues that can be sampled from ({len(basic)}).\n    Decrease the number of charges or check PDB. Are you using -R/H/K to exclude residues?")
+        exit(1)
 
     sample = charge_sample(mol=basic,
                            n_charges=args.n_charges,
